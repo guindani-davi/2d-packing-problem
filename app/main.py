fc04430fdfcb0d65ac7920dd524e8fe340c84dde
Davi-Guindani <davigpvieira@gmail.com>
2025-05-13 13:45:22 -0300

    only inputs w/ 200+ rects

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from helpers import read_input_file
 
 
 def main():
-    input_file = "empacotamentos/empacotamento_medio.txt"
+    input_file = "inputs/packing_1.txt"
     alpha = 0.3
     bin_width, bin_height, rectangles = read_input_file(input_file)
     
