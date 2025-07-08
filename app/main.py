6c5a50c00b3d3e73387a045b806763396d3e5835
Gabriel Knust <gabrielsilvaknustws@gmail.com>
2025-07-08 15:04:56 -0300

    feat: Busca Tabu

--- app/main.py
+++ app/main.py
@@ -6,45 +6,17 @@ from helpers import read_input_file
 import time
 
 def main():
-    numbers = [2]
+    numbers = [3] # define qual arquivo de entrada será processado (Arquivos da pasta inputs)
     for number in numbers:
         input_file = f"./app/inputs/packing_{number}.txt"
-        alpha = 0.5
+        alpha = 0
         bin_width, bin_height, rectangles = read_input_file(input_file)
-        rectanglesCopy = rectangles.copy()
-        current_solution = GraspSolution(Bin(bin_width, bin_height), alpha, number)
-        tempoExecucao = time.time() + 60 * 5  # 5 minutos
-        bestSolution = GraspBuilder.build(current_solution, rectangles)
-        bestValue = objective_function(bestSolution)
-        tabuList = []
-        maxTabuSize = round(len(rectanglesCopy) * 0.3)
-        while time.time() < tempoExecucao:
-            list = {}
-            current_solution = GraspBuilder.build(current_solution, rectangles)
-            print("Current solution:")
-            print(current_solution)
-            print("Objective function value:", objective_function(current_solution))
-            neighborhood = GraspBuilder.neighborhood(current_solution, rectanglesCopy)
-            for neighborhood_solution in neighborhood: #loop para avaliar todas as solucoes vizinhas e adicionar em uma lista
-                list.setdefault(objective_function(neighborhood_solution), neighborhood_solution)
-            list = sorted(list.items())
-            for key, value in list:
-                if key not in tabuList:
-                    tabuList.append(key)
-                    current_solution = value
-                    if key < bestValue:
-                        bestSolution = current_solution
-                        bestValue = key
-                        print("New best solution found:")
-                        print(bestSolution)
-                        print("Objective function value:", bestValue)
-                        #função de aspiração confusa
-                    if len(tabuList) >= maxTabuSize:
-                        tabuList.pop(0)
-                    break
-        print("Final best solution:")
-        print(bestSolution)
-        print("Objective function value:", bestValue)
+        solution = GraspSolution(Bin(bin_width, bin_height), alpha, number)
+        maxTabuSize = round(len(rectangles) * 0.3)
+        best_solution, best_value_ = GraspBuilder.tabuSearch(solution, rectangles, maxTabuSize)
+        print("Best solution found:")
+        print(best_solution)
+        print("Best objective function value", best_value_)
             
         
 
--- app/models/grasp_builder.py
+++ app/models/grasp_builder.py
@@ -6,6 +6,7 @@ from models.solution import Solution
 from models.bin import Bin
 from models.rectangle import Rectangle
 from models.objective_function import objective_function
+import time
 
 class GraspBuilder(ABC):
     @staticmethod
@@ -83,13 +84,68 @@ class GraspBuilder(ABC):
         return best_solution,best_solution_rectangles
     
     @staticmethod
-    def neighborhood(solution: GraspSolution, rectangles: list[Rectangle]) -> list[GraspSolution]:
-        neighbors = []
-        for i in range(len(rectangles)):
-            for j in range(i + 1, len(rectangles)):
-                current_rectangles = copy.deepcopy(rectangles)
-                current_rectangles[j], current_rectangles[i] = current_rectangles[i], current_rectangles[j] # é isso mesmo?
-                new_solution = copy.deepcopy(solution)
-                new_solution = GraspBuilder.build(new_solution, list(current_rectangles))
-                neighbors.append(new_solution)
-        return neighbors
+    def addTabu(pair: tuple[int, int],tabu_list: list,maxTabuSize: int) -> None:
+        if pair in tabu_list:
+            tabu_list.remove(pair)
+        tabu_list.append(pair)
+        if len(tabu_list) >= maxTabuSize:
+            tabu_list.pop(0)
+    
+    @staticmethod
+    def tabuSearch(solution: GraspSolution, rectangles: list[Rectangle],maxTabuSize: int) -> list[GraspSolution]:
+        tabu_list = []
+        current_solution = GraspBuilder.build(copy.deepcopy(solution), copy.deepcopy(rectangles))
+        current_value_s = objective_function(current_solution)
+        best_solution = copy.deepcopy(solution)
+        best_value_ = current_value_s
+        print("Starting tabu search with initial solution:", current_solution)
+        print("Initial objective function value:", current_value_s)
+        best_neighbor_value = float('inf')
+        best_neighbor_solution = None
+        best_neighbor_move = (0, 0)
+        reset = False
+        tempoExecucao = time.time() + 60 * 5  # 5 minutos
+        while True:
+            for i in range(len(rectangles)-1):
+                for j in range(i + 1, len(rectangles)):
+                    if time.time() > tempoExecucao:
+                        print("Tempo de execução excedido. Encerrando busca tabu.")
+                        return best_solution, best_value_
+                    current_rectangles = copy.deepcopy(rectangles)
+                    current_rectangles[j], current_rectangles[i] = current_rectangles[i], current_rectangles[j]
+                    new_solution = GraspBuilder.build(copy.deepcopy(solution), list(current_rectangles))
+                    new_value = objective_function(new_solution) #avalia nova solução
+                    print(f"Evaluating swap ({i}, {j}): New value = {new_value}, Current best value = {best_value_}")
+                    print(f"Current solution: {new_solution}")
+                    if (i,j) in tabu_list: # se a troca já está na tabu
+                        if new_value < best_value_: # verifica critério de aspiração (nova solução é melhor que a melhor solução encontrada até agora)
+                            best_value_ = new_value # atualiza melhor valor
+                            best_solution = copy.deepcopy(new_solution) # atualiza melhor solução
+                            current_value_s = new_value # atualiza valor da solução atual
+                            current_solution = copy.deepcopy(new_solution) # atualiza solução atual
+                            GraspBuilder.addTabu((i,j), tabu_list,maxTabuSize) # passa a troca para o final da tabu list
+                            reset = True # flag para reiniciar a busca
+                            break
+                    else: # se a troca não está na tabu
+                        if new_value < current_value_s: #se a nova solução é melhor que a solução atual
+                            current_value_s = new_value # atualiza valor da solução atual
+                            current_solution = copy.deepcopy(new_solution) # atualiza solução atual
+                            if new_value < best_value_: # se a nova solução é melhor que a melhor solução
+                                best_value_ = new_value # atualiza melhor valor
+                                best_solution = copy.deepcopy(new_solution) # atualiza melhor solução
+                                GraspBuilder.addTabu((i,j), tabu_list, maxTabuSize) # passa a troca para o final da tabu list
+                                print("Found a better solution:", best_solution)
+                                print("Objective function value:", best_value_)
+                            reset = True
+                            break
+                        else:
+                            if new_value < best_neighbor_value:
+                                best_neighbor_value = new_value
+                                best_neighbor_solution = copy.deepcopy(new_solution)
+                                best_neighbor_move = (i, j)
+                if reset:
+                    break
+            if not reset:
+                current_solution = best_neighbor_solution
+                current_value_s = best_neighbor_value
+                GraspBuilder.addTabu(best_neighbor_move, tabu_list, maxTabuSize)
